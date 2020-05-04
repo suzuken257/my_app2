1c9441b90ef1a662bb2e926129d6e60e464430df
suzuken257 <60838938+suzuken257@users.noreply.github.com>
2020-05-04 19:27:31 +0900

    save

--- db/migrate/20200503153707_create_comments.rb
+++ db/migrate/20200503153707_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :post_id
-      t.text :text
+      t.integer :user_id, foreign_key: true
+      t.integer :post_id, foreign_key: true
+      t.text :text, null: false
       t.timestamps
     end
   end
